6752ce42a5337da14c71f59f5b9333e3a2ac4b5e
Bishesh99 <bisheshsingh.99@gmail.com>
2023-09-28 19:37:06 +0530

    Abstract Factory Implementations + tests

---
+++ src/main/java/org/design/patterns/AbstractFactory/Constants/FoodTypes.java
@@ -0,0 +1,15 @@
+package org.design.patterns.AbstractFactory.Constants;
+
+public class FoodTypes {
+    public enum StarterType {
+        FRENCH_FRIES, ROLL;
+    }
+
+    public enum MainCourseType {
+        BURGER, PIZZA;
+    }
+
+    public enum DessertType {
+        CHOCO_LAVA_CAKE, ICE_CREAM;
+    }
+}
---
+++ src/main/java/org/design/patterns/AbstractFactory/Models/Starter.java
@@ -0,0 +1,8 @@
+package org.design.patterns.AbstractFactory.Models;
+
+import lombok.Data;
+
+@Data
+public abstract class Starter {
+    protected final String name;
+}
