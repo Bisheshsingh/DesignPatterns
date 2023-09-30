87e56e069587a9d900377ea9398d7c486ecab733
Bishesh99 <bisheshsingh.99@gmail.com>
2023-09-30 13:39:38 +0530

    Using getter instead of manual code

--- src/main/java/org/design/patterns/Behavioral/ChainOfResponsibility/Models/Handler.java
+++ src/main/java/org/design/patterns/Behavioral/ChainOfResponsibility/Models/Handler.java
@@ -1,14 +1,13 @@
 package org.design.patterns.Behavioral.ChainOfResponsibility.Models;
 
+import lombok.Getter;
+
+@Getter
 public abstract class Handler {
     private Handler nextHandler;
 
     public Handler setNextHandler(final Handler nextHandler) {
         this.nextHandler = nextHandler;
-        return nextHandler;
-    }
-
-    public Handler getNextHandler() {
         return nextHandler;
     }
 
